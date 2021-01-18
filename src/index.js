6eda0741b3d40e835e049fbb41efd6acfab56c9f
Or Paz <orpaz007@gmail.com>
2021-01-18 23:30:00 +0200

    async/await user and task requests

--- src/index.js
+++ src/index.js
@@ -1,15 +1,131 @@
-const express = require('express')
-const Tasks = require('./models/task')
-const User = require("./models/user")
-require('./db/mongoose')
+// const express = require('express')
+// const Tasks = require('./models/task')
+// const User = require("./models/user")
+// require('./db/mongoose')
 
-// const { request } = require('https')
+// // const { request } = require('https')
+
+// const app = express()
+// const port = process.env.PORT || 3000
+
+
+
+
+// app.use(express.json())
+
+// app.post('/users', async (req, res) => {
+//     const user = new User(req.body)
+
+//     try {
+//         await user.save()
+//         res.status(201).send(user)
+//     } catch (e) {
+//         res.status(400).send(e)
+//     }
+// })
+
+// app.get('/users', async (req, res) => {
+//     try {
+//         const users = await User.find({})
+//         res.send(users)
+
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
+
+// app.get('/users/:id', async (req, res) => {
+//     const _id = req.params.id
+
+//     try {
+//         const user = await User.findById(_id)
+
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+
+//         res.send(user)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
+
+
+// // app.get('/users/:id', (req, res) => {
+// //     const _id = req.params.id
+// //     User.findById(_id).then((user) => {
+// //         if (!user) {
+// //            return res.status(404).send()
+// //         }
+// //     res.send(user)
+// //     }).catch((e) => {
+// //         res.status(500).send()
+// //      })
+// //  })
+
+// app.post('/tasks', async(req, res) => {
+//     // console.log(req.body)
+//     // res.send("test")
+//     try{
+//         const task = new Tasks(req.body)
+//         await task.save()
+//         res.status(201).send(task)
+//     } catch(e){
+//         res.status(400).send(e)
+//     }
+//     // const task = new Tasks(req.body)
+//     // task.save().then(() => {
+//     //     res.status(201).send(task)
+//     // }).catch((e) => {
+//     //     res.status(400).send(e)
+//     //  })
+// })
+
+
+// app.get('/tasks',async(req,res) =>{
+//     try {
+//         const tasks = await Tasks.find({})
+//         res.send(tasks)
+//     } catch(e){
+//         res.status(500).send()
+//     }
+//     // Tasks.find({}).then((tasks)=>{
+//     //     res.send(tasks)
+//     // }).catch((e) =>{
+//     //     res.status(500).send()
+//     // })
+// })
+
+// app.get('/tasks/:id', async (req, res) => {
+//     const _id = req.params.id
+//     try {
+//         const task = await Task.findById(_id)
+
+//         if (!task) {
+//             return res.status(404).send()
+//         }
+//         res.send(task)
+//     } catch (e) {
+//         res.status(500).send()
+        
+//     }
+// })
+
+
+
+// app.listen(port, ()=>{
+//     console.log('Server is up port ' + port)
+// })
+
+
+
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
-
-
-
 
 app.use(express.json())
 
@@ -49,37 +165,24 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
 
-// app.get('/users/:id', (req, res) => {
-//     const _id = req.params.id
-//     User.findById(_id).then((user) => {
-//         if (!user) {
-//            return res.status(404).send()
-//         }
-//     res.send(user)
-//     }).catch((e) => {
-//         res.status(500).send()
-//      })
-//  })
-
-app.post('/tasks', (req, res) => {
-    // console.log(req.body)
-    // res.send("test")
-    const task = new Tasks(req.body)
-    task.save().then(() => {
+    try {
+        await task.save()
         res.status(201).send(task)
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e)
-     })
+    }
 })
 
-
-app.get('/tasks',(req,res) =>{
-    Tasks.find({}).then((tasks)=>{
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((e) =>{
+    } catch (e) {
         res.status(500).send()
-    })
+    }
 })
 
 app.get('/tasks/:id', async (req, res) => {
@@ -98,6 +201,6 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
-app.listen(port, ()=>{
-    console.log('Server is up port ' + port)
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
