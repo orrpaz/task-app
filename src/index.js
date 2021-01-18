8589e616a2941b8b55719bb9e912f523a95661e8
Or Paz <orpaz007@gmail.com>
2021-01-18 21:16:58 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const Tasks = require('./models/task')
+const User = require("./models/user")
+require('./db/mongoose')
+
+// const { request } = require('https')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.listen(port, ()=>{
+    console.log('Server is up port ' + port)
+})
+
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+    console.log(req.body)
+    // res.send("test")
+    const user = new User(req.body)
+    user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+     })
+})
+
+app.get('/users',(req,res) =>{
+    User.find({}).then((users)=>{
+        res.send(users)
+    }).catch((e) =>{
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user) {
+           return res.status(404).send()
+        }
+    res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+     })
+ })
+
+app.post('/tasks', (req, res) => {
+    // console.log(req.body)
+    // res.send("test")
+    const task = new Tasks(req.body)
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+     })
+})
+
+
+app.get('/tasks',(req,res) =>{
+    Tasks.find({}).then((tasks)=>{
+        res.send(tasks)
+    }).catch((e) =>{
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Tasks.findById(_id).then((task) => {
+        if (!task) {
+           return res.status(404).send()
+        }
+    res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+     })
+ })
