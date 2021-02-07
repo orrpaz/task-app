ffdfb1d01867dd8c9ae91aa4e24d9710835684ed
Or Paz <orpaz007@gmail.com>
2021-02-07 15:54:53 +0200

    suppurt login and logout with authentication

--- src/models/user.js
+++ src/models/user.js
@@ -1,46 +1,73 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            required: true,
-            trim: true,
-            lowercase: true,
-            validate(value) {
-                if (!validator.isEmail(value)) {
-                    throw new Error('Email is invalid')
-                }
-            }
-        },
-        password: {
-            type: String,
-            required: true,
-            minlength: 7,
-            trim: true,
-            validate(value) {
-                if (value.toLowerCase().includes('password')) {
-                    throw new Error('Password cannot contain "password"')
-                }
-            }
-        },
-        age: {
-            type: Number,
-            default: 0,
-            validate(value) {
-                if (value < 0) {
-                    throw new Error('Age must be a postive number')
-                }
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
             }
         }
-    })
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+})
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
+userSchema.methods.toJSON =  function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
@@ -58,6 +85,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
     return user
 }
 
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this
 
@@ -67,6 +95,7 @@ userSchema.pre('save', async function (next) {
 
     next()
 })
-const User = mongoose.model('User',userSchema)
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,26 +1,58 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
-
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.get('/users', async (req, res) => {
+router.post('/users/login', async (req, res) => {
     try {
-        const users = await User.find({})
-        res.send(users)
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (e) {
-        res.status(500).send()
+        res.status(400).send()
     }
+})
+
+router.post('/users/logout',auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+
+    }
+})
+
+router.post('/users/logoutAll',auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+         
+        await req.user.save()
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+
+    }
+
+})
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
@@ -39,7 +71,6 @@ router.get('/users/:id', async (req, res) => {
     }
 })
 
-
 router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
@@ -50,15 +81,11 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-
         const user = await User.findById(req.params.id)
 
         updates.forEach((update) => user[update] = req.body[update])
         await user.save()
-        
-        // const user = await User.findByIdAndUpdate(req.params.id, 
-        //     req.body, { new: true, runValidators: true })
-    
+
         if (!user) {
             return res.status(404).send()
         }
@@ -69,21 +96,18 @@ router.patch('/users/:id', async (req, res) => {
     }
 })
 
-
 router.delete('/users/:id', async (req, res) => {
-
-
-    try{
+    try {
         const user = await User.findByIdAndDelete(req.params.id)
 
         if (!user) {
             return res.status(404).send()
         }
+
         res.send(user)
     } catch (e) {
-        res.status(500).send(e)
+        res.status(500).send()
     }
 })
-
 
 module.exports = router
